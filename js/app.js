bf8b18b716febc5740f80b0f9d410d0875fe4446
Alex Martin <martin.alex.t+github@gmail.com>
2015-01-29 01:14:46 -0500

    Now With (Useless) Caching I'm committing here for posterity. I implemented a long caching method into my data handler that I was very proud of, only to realize after laying it all out that the entire system suffered from a race condition that made the caching effectively useless. It's late, I'm learning a lot, and I'm falling back on live queries at the expense of added HTTP requests for now. Life lesson.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,22 @@
 // Globals
 var rootUrl = "https://api.github.com/users/ATMartin";
 
-
-
-var myTemplate = function(endpoint, templateName, dest) {
+var myTemplate = function(endpoint, templateName, dest, cache) {
   this._url = rootUrl + (endpoint != ''?'/':'') + endpoint + "?access_token=" + window.token;
 	this._template = _.template($('[data-template-name='+ templateName + ']').text());
 	this._destination = $(dest);
-	return this;
+	// For now, cache ALL requests up front.
+	// Return in a callback to prevent race condition where cache doesn't exist on return.
+	this.cache(function() { return this; });
 };
 //  |-- Proto
 myTemplate.prototype = { 
   _url: '',
 	_template: '',
 	_destination: '',
+	_cachedData: null,
 	data: function(callback) {
-    $.ajax({
+		$.ajax({
 	    url: this._url,
 	    type: "GET"
 		})
@@ -26,17 +27,31 @@ myTemplate.prototype = {
 			callback(data);
 		});
 	},
-	render: function() {
+	cache: function(callback) {
 		var me = this;
-		this.data(function(d) {
-			if (d.length > 1) {
-				d.forEach(function(item) {
-					me._destination.append(me._template(item));
+	  this.data(function(d) {
+		  me._cachedData = d;
+			callback();
+	  });
+	},
+	getCached: function() { 
+		return this._cachedData || 'No cache on this object'; 
+	},
+	render: function() {
+		var me = this;                              
+		var doRender = function(data, context) {
+			if (!context) { context = me; }  //context MUST be set so we can check for _cachedData.
+			if (data.length > 1) {
+				data.forEach(function (item) {
+					context._destination.append(context._template(item));
 				});
 			} else {
-				me._destination.append(me._template(d)); 
+				context._destination.append(context._template(data));
 			}
-		});
+		};
+		// Minimize HTTP requests by checking our object's cache first.
+		if (this._cachedData) { doRender(this._cachedData, this); }
+		else { this.data(doRender); } 
 	}
 };
 
@@ -45,8 +60,10 @@ myTemplate.prototype = {
 var bigRepoListing = new myTemplate('repos', 'big-repo-listitem', '.repo-big-list');
 //  |-- Sidebar & Account Data 
 var mySidebarData = new myTemplate('', 'sidebar-content', '.sidebar');
+//  |-- Starred Details
+var myStarredRepos = new myTemplate('starred', 'starred-repos', '');
+// HELPER VARIABLES
+//	|-- Stars Count (for Sidebar Profile) 
 
-//myAccountData.render();
 bigRepoListing.render();
-
-
+mySidebarData.render();
